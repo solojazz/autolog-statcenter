d0d96ec2d572e58bdccb027bf4e1a28ef04216f4
andihow <andihow@gmail.com>
2017-01-22 15:03:51 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+To Install: 
+ 1. Create directory to store bot & dependencies
+ 2. git clone https://github.com/autolog-statbot/autolog-statcenter.git
+ 3. npm install
+ 
+To Run: 
+ 1. node autolog.js
---
+++ autolog.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var newUsers = new Discord.Collection();
+
+
+//14 words for roasty
+client.on("message", msg => {
+    if (msg.content.includes("14 words", "14 Words")) {
+        msg.channel.sendMessage("We must secure the existence of our people and a future for white children.");
+    }
+});
+
+//announce user who's entered
+client.on("guildMemberAdd", (member) => {
+    console.log(`New User "${member.user.username}" has entered the room.` );
+    member.guild.defaultChannel.sendMessage(`"${member.user.username}" has entered the room.`);
+});
+
+//annoutnce user who's left
+client.on("guildMemberRemove", (member) => {
+  console.log(`New User "${member.user.username}" has left the room. `);
+  member.guild.defaultChannel.sendMessage(`"${member.user.username}" has left the room.`)
+})
+
+//"I am alive" message for cli
+client.on('ready', () => {
+	console.log('I am running!!');
+});
+
+
+
+client.login("MjcyNjYzODAwNzkxMTcxMDcz.C2YXag.E3QfYe6WqrNLc9FyNG99pml7Hhw")
